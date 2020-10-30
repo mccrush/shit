a13e564c253e74bb7267e519ef227f9bc3ba2c3a
mccrush <mccrush2027@gmail.com>
2020-10-30 12:53:22 +0700

    Add show hide items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("All work!");
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  console.log("All work!")
+  showItem()
+
+
+  function showItem() {
+    let itemHead = document.querySelectorAll('.item-head')
+
+    for (let item of itemHead) {
+      item.addEventListener('click', () => {
+        item.nextElementSibling.classList.toggle("show-item")
+        if (item.nextElementSibling.classList.contains("show-item")) {
+          item.nextElementSibling.style.height = '290px'
+        } else {
+          item.nextElementSibling.style.height = 0
+        }
+
+      })
+    }
+  }
+
+
+
+}, false);
