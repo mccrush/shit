8530b51e152a5d6714d937ba1c0ea73f3910631c
mccrush <mccrush2027@gmail.com>
2020-12-30 21:51:53 +0700

    Settings font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar">
     <div class="container-fluid justify-content-center">
-      <h1>Electro ⚡ Shit</h1>
+      <h1>Electro <small>⚡</small> Shit</h1>
     </div>
   </header>
 </template>
