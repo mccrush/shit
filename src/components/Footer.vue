858ca0c943d68bac050eab5e47f734f6b7860093
mccrush <mccrush2027@gmail.com>
2020-12-31 10:01:31 +0700

    Settings Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,23 @@
 <template>
-  <footer class="footer pt-2 text-center">
-    <small>&copy; 2021 «Electro Shit»</small>
+  <footer class="footer text-center border-top mt-3 pt-2">
+    <small
+      >&copy; 2021 «Electro Shit»&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+      <a
+        href="https://github.com/mccrush/shit"
+        target="_blank"
+        title="Fork on Github"
+        >Fork on Github</a
+      ></small
+    >
   </footer>
 </template>
+
+<style scoped>
+a {
+  color: #212529;
+}
+
+a:hover {
+  text-decoration: none;
+}
+</style>
