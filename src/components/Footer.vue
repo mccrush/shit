1c37cd0b697b20ea2577fa38de2f43649e827303
mccrush <mccrush2027@gmail.com>
2020-12-31 10:54:36 +0700

    Add padding

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer text-center border-top mt-3 pt-2">
+  <footer class="footer text-center border-top mt-3 pt-2 pb-2">
     <small
       >&copy; 2021 «Electro Shit»&nbsp;&nbsp;&bull;&nbsp;&nbsp;
       <a
