ec91df5329a88dbfd297483f61438cb4c13120d3
mccrush <mccrush2027@gmail.com>
2021-01-08 14:29:57 +0700

    Build proj

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,25 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  console.log("All work!")
-
-  // Check that service workers are supported
-  if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('js/service-worker.js');
-    });
-  }
-
-  showItem()
+//document.addEventListener('DOMContentLoaded', function () {
 
 
-  function showItem() {
-    let itemHead = document.querySelectorAll('.item-head')
+console.log("JS work!")
 
-    for (let item of itemHead) {
-      item.addEventListener('click', () => {
-        item.nextElementSibling.classList.toggle("show-item")
-        if (item.nextElementSibling.classList.contains("show-item")) {
-          item.nextElementSibling.classList.add('height-auto')
-          item.children[1].classList.add('item-head-rotate')
-        } else {
-          item.nextElementSibling.classList.remove('height-auto')
-          item.children[1].classList.remove('item-head-rotate')
-        }
-
-      })
-    }
-  }
+// Check that service workers are supported
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('js/service-worker.js')
+//       .then(reg => console.log('service worker registered'))
+//       .catch(err => console.log('service worker not registered', err))
+//   });
+// }
 
 
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  navigator.serviceWorker.register('service-worker.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err))
+}
 
-}, false);
+//}, false);
